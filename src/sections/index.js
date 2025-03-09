c4bb2198f71c33bc48ac6f7fa0ad687050756d9d
Ankit Mishra <ankit.kumar.mishra2006@gmail.com>
2025-03-09 07:43:48 +0530

    Navbar Done

---
+++ src/sections/index.js
@@ -0,0 +1,12 @@
+import Footer from "./Footer";
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import CustomerReviews from "./CustomerReviews";
+import SuperQuality from "./SuperQuality";
+
+export {
+    Hero, Footer, PopularProducts, Services, SpecialOffer, Subscribe, CustomerReviews, SuperQuality
+}
